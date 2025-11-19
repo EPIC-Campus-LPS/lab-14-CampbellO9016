24743d26e96331628e70882d096da484d134d3b4
CampbellO9016 <campbellosher@icloud.com>
2025-11-19 11:59:44 -0700

    battle ship vaildtor done but balance not

--- battleship/Validate.java
+++ battleship/Validate.java
@@ -3,69 +3,71 @@ package battleship;
 public class Validate {
 	public static boolean boardValidator(String[][] board) {
 		boolean yes = false;
-		boolean R = false;
-		boolean C = false;
-		boolean B = false;
-		boolean D = false;
-		boolean S = false;
+		int R = 0;
+		int C = 0;
+		int B = 0;
+		int D = 0;
+		int S = 0;
 		for(int i = 0; i < board.length; i++) {
 			for(int j = 0; j < board[i].length; j++) {
-				if(board[i][j].equals("C")&& !C) {
-					if(board[i][j+4].equals("C")) {
+				if(board[i][j].equals("C")&& i + 4 < 10 && j + 4 < 10) {
+					if(C <= 1 && board[i][j+4].equals("C")) {
 						yes = true;
-						C = true;
+						C ++;
 					}
-					else if(board[i+4][j].equals("C")) {
+					else if(C <= 1 && board[i+4][j].equals("C")) {
 						yes = true;
-						C = true;
-					}
-					else
-						yes = false;	
-				}
-				if(board[i][j].equals("B")&& !B) {
-					if(board[i][j+2].equals("B")) {
-						yes = true;
-						B = true;
-					}
-					else if(board[i+2][j].equals("B")) {
-						yes = true;
-						B = true;
+						C ++;
 					}
 					else
 						yes = false;	
 				}
-				if(board[i][j].equals("R")&& !R) {
-					if(board[i][j+2].equals("R")) {
+				if(board[i][j].equals("B")&& i + 3 < 10 && j + 3 < 10) {
+					if( B <= 1 &&board[i][j+3].equals("B")) {
 						yes = true;
-						R = true;
+						B ++;
 					}
-					else if(board[i+2][j].equals("R")) {
+					else if(B <= 1 &&board[i+3][j].equals("B")) {
 						yes = true;
-						R = true;
-					}
-					else
-						yes = false;	
-				}
-				if(board[i][j].equals("S")&& !S) {
-					if(board[i][j+2].equals("S")) {
-						yes = true;
-						S = true;
-					}
-					else if(board[i+2][j].equals("S")) {
-						yes = true;
-						S = true;
+						B ++;
 					}
 					else
 						yes = false;	
 				}
-				if(board[i][j].equals("D")&& !D) {
-					if(board[i][j+2].equals("D")) {
+				if(board[i][j].equals("R")&& i + 2 < 10 && j + 2 < 10) {
+					if(R <= 1 && board[i][j+2].equals("R")) {
 						yes = true;
-						D = true;
+						R ++;
 					}
-					else if(board[i+2][j].equals("D")) {
+					else if(R <= 1 &&board[i+2][j].equals("R")) {
 						yes = true;
-						D = true;
+						R ++;
+					}
+					else 
+						yes = false;
+					
+						
+				}
+				if(board[i][j].equals("S")) {
+					if(j + 2 < 10 && S <= 1 &&board[i][j+2].equals("S")) {
+						yes = true;
+						S ++;
+					}
+					else if(i + 2 < 10 && S <= 1 &&board[i+2][j].equals("S") ) {
+						yes = true;
+						S ++;
+					}
+					else
+						yes = false;	
+				}
+				if(board[i][j].equals("D")&& i + 1 < 10 && j + 1 < 10) {
+					if(D <= 1 &&board[i][j+1].equals("D")) {
+						yes = true;
+						D ++;
+					}
+					else if(D <= 1 &&board[i+1][j].equals("D")) {
+						yes = true;
+						D ++;
 					}
 					else
 						yes = false;	
@@ -73,6 +75,29 @@ public class Validate {
 				
 			}
 		}
+		if(C+B+R+S+D == 5) {
+			yes = true;
+		}
+		else
+			yes = false;
 		return yes;
+	}
+	public static boolean balanced(String[][] board) {
+		int count = 0;
+		for(int i = 0; i < board.length; i++) {
+			for(int j = 0; j < board[i].length; j++) {
+				if(!(board[i][j].equals("0"))) {
+					if() {
+						
+					}
+					count ++;
+				}
+			}
+		}
+		
+		
+		
+		
+		return true;
 	}
 }
--- battleship/ValidateRunner.java
+++ battleship/ValidateRunner.java
@@ -19,21 +19,30 @@ public class ValidateRunner {
 		int count2 = 0;
 		String[][] board1 = new String[10][10];
 		boolean yes = true;
+		boolean yes2 = true;
+		boolean yes3 = true;
+		int row = 0;
 		while(scan1.hasNextLine()) {
 			line = scan1.nextLine();
-			int row = 0;
 			String[] row1 = line.split(",");
 			if(row1.length == 10) {
-				for(int col = 0; col < row1.length - 1; col++) {
-					board1[col][row] = row1[col];
+				for(int col = 0; col < row1.length; col++) {
+					board1[row][col] = row1[col];
 				}
 					row ++;
 			}
-			else
+			else {
 				yes = false;
-			
+			}
+			count2++;
 		}
-		count2++;
+//		for(String[] n: board1) {
+//			for(String n1: n) {
+//				System.out.print(n1);
+//			}
+//			System.out.println();
+//		}
+		
 		if(count2 != 10) {
 			yes =false;
 		}
@@ -43,8 +52,20 @@ public class ValidateRunner {
 		if(yes) {
 			System.out.println("This board is Valid.");
 		}
-		else
+		else {
 			System.out.println("This board is not Valid.");
+			yes2 = false;
+		}
+		
+		if(yes2) {
+			yes3 = Validate.balanced(board1);
+			
+			if(yes3)
+				System.out.println("This board is Balanced.");
+			else
+				System.out.println("This board is not Balanced.");
+				
+		}
 
 		
 		
